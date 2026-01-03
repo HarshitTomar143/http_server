c8f5f7e6b678f881140b81ebe88623e03e44fa7e
Harshittomar <tomarharshit2023@gmail.com>
2026-01-04 01:22:58 +0530

    Adding the whole logic into handler file

--- handler.go
+++ handler.go
@@ -4,17 +4,52 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strings"
 )
 
 func handleConnection(conn net.Conn){
 	defer conn.Close()
 
-	fmt.Println("New CLient Connected: ", conn.RemoteAddr())
+	reader := bufio.NewReader(conn)
+	fmt.Println("New client connected:",conn.RemoteAddr())
 
-	reader:= bufio.NewReader(conn)
+	//Read the request line 
+	requestLine, err := reader.ReadString('\n')
+	if err != nil {
+		return
+	}
 
-	req := parseRequest(reader)
-	res := handleRoute(req)
+	parts := strings.Split(strings.TrimSpace(requestLine), " ")
+	method := parts[0]
+	path := parts[1]
 
-	writeResponse(conn, res)
+	fmt.Println("Method:", method)
+	fmt.Println("Path:", path)
+
+	//consume remaining headers
+	for {
+		line,_ := reader.ReadString('\n')
+		if (line == "\r\n"){
+			break
+		}
+	}
+
+	//routing
+	var body string
+	if path == "/" {
+		body = "<h1>Home Page</h1>"
+	} else if path == "/contact" {
+		body = "<h1>Contact Page</h1>"
+	} else {
+		body = "<h1>404 Not Found</h1>"
+	}
+
+	response := "HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/html\r\n" +
+		fmt.Sprintf("Content-Length: %d\r\n", len(body)) +
+		"\r\n" +
+		body
+
+	conn.Write([]byte(response))
+	
 }
